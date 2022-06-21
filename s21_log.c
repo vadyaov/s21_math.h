c87af95b83b9bf0d8988e8b1d10d636065031d66
Neely Arline <neelyarl@su-d5.msk.21-school.ru>
2022-06-21 23:19:56 +0300

    log

--- s21_log.c
+++ s21_log.c
@@ -1,4 +1,5 @@
 #include "s21_math.h"
+#include <stdio.h>
 
 /**
  * @brief 
@@ -7,5 +8,34 @@
  * @return long double 
  */
 long double s21_log(double x) {
-
+    if (!x || s21_fabs(x) < EPS) return -S21_HUGE_VAL;
+    if (S21_IS_INF(x) == 1) return S21_HUGE_VAL;
+    if (x == 2) return S21_LN2;
+    if (s21_fabs(x - 1.0) < EPS) return 0.0L;
+    if (s21_fabs(x - S21_ME) <= 1e-15) return 1.0L;
+    if (__builtin_signbit(x) || S21_IS_INF(x) == -1) return S21_NAN_VAL;
+    long double res = 0.0L;
+    if (x < (2.0 - EPS)) {
+        long double num = (long double)x - 1.0L;
+        long double sign = 1.0L;
+        long double delim = 1.0L;
+        long double ch = num;
+        while (ch > 1E-25) { // 0 < x < 2
+            res += ch / delim * sign;
+            ch *= num;
+            sign *= -1.0L;
+            delim += 1.0L;
+        }      
+    } else {
+        long double del = 0.0L;
+        long double how_much = 0.0L;
+        while (x - 2.0L > EPS) {
+            del = x / 2.0L;
+            x /= 2.0L;
+            how_much++;
+        }
+        res = how_much *S21_LN2 + s21_log(del);
+    }
+    
+    return res;
 }
--- s21_math.h
+++ s21_math.h
@@ -1,8 +1,11 @@
 #ifndef SRC_S21_MATH_H
 #define SRC_S21_MATH_H
 
+#define EPS 1E-6
+
 #define S21_M_PI 3.14159265358979323846264338327950288
 #define S21_ME 2.71828182845904523536028747135266250
+#define S21_LN2 0.693147180559945309417232121458176568
 
 // https://gcc.gnu.org/onlinedocs/gcc-4.5.0/gcc/Other-Builtins.html
 
@@ -27,6 +30,7 @@
 int s21_abs(int x);
 long double s21_fabs(double x);
 long double s21_exp(double x);
+long double s21_log(double x);
 long double s21_pow(double base, double exp);
 long double s21_sin(double x);
 long double s21_cos(double x);
