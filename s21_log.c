5737deb452fb7d022c3aa9e7098980eb71d91c7f
neelyarl <neelyarl@student.21-school.ru>
2022-06-22 00:36:12 +0300

    upd log

--- s21_log.c
+++ s21_log.c
@@ -11,7 +11,7 @@ long double s21_log(double x) {
     if (!x || s21_fabs(x) < EPS) return -S21_HUGE_VAL;
     if (S21_IS_INF(x) == 1) return S21_HUGE_VAL;
     if (x == 2) return S21_LN2;
-    if (s21_fabs(x - 1.0) < EPS) return 0.0L;
+    if (s21_fabs(x - 1.0) < EPS/10) return 0.0L;
     if (s21_fabs(x - S21_ME) <= 1e-15) return 1.0L;
     if (__builtin_signbit(x) || S21_IS_INF(x) == -1) return S21_NAN_VAL;
     long double res = 0.0L;
@@ -27,14 +27,12 @@ long double s21_log(double x) {
             delim += 1.0L;
         }      
     } else {
-        long double del = 0.0L;
         long double how_much = 0.0L;
         while (x - 2.0L > EPS) {
-            del = x / 2.0L;
             x /= 2.0L;
             how_much++;
         }
-        res = how_much *S21_LN2 + s21_log(del);
+        res = how_much *S21_LN2 + s21_log(x);
     }
     
     return res;
