b521f7ffc7932f4135172963db1b767c3d947832
Neely Arline <neelyarl@su-d5.msk.21-school.ru>
2022-06-26 17:33:18 +0300

    some fix

--- s21_fmod.c
+++ s21_fmod.c
@@ -9,14 +9,13 @@
  */
 long double s21_fmod(double x, double y) {
     if (!y) return S21_NAN_VAL;
+    y = s21_fabs(y);
     if (x > 0) {
-        if (__builtin_signbit(y)) y *= -1.0;
         while (x >= y) {
             x -= y;
         }    
     }
     if (x < 0) {
-        if (__builtin_signbit(y)) y *= -1.0;
         while (x <= 0) {
             x += y;
         }
